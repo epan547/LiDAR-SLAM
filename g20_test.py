c2abe71699f2c2085e51f53821e32b97b60a4621
Eamon <eamon@piratecat.com>
2020-12-16 15:38:28 -0500

    Oops

--- g20_test.py
+++ g20_test.py
@@ -1,5 +1,9 @@
 import numpy as np
 import g2o
 import os
+import pickle
 
-optimizer =g2o.PoseGraphOptimization()
+optimizer = g2o.SparceOptimizer()
+load_file = open('data','rb')
+data = pickle.load(load_file)
+print(data)
