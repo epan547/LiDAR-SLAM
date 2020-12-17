a60325b86ebc1d4984b9aa31d84a80f56ecc3a28
Eamon <eamon@piratecat.com>
2020-12-16 23:41:30 -0500

    New data

--- g2o_test.py
+++ g2o_test.py
@@ -73,6 +73,8 @@ class PoseGraphOptimization(g2o.SparseOptimizer):
                 # Add edges between current odom point and all corresponding lidar points
                 start_index = int((f/2) * 361)
                 for x in range(361):
+                    print("F/2 = " + f/2)
+                    print("X = " + x)
                     lidar_pt = lidar[int(f/2)][x][0]
                     # lidar_pt = vertices[int(x+start_index)].estimate()
                     # lidar_pt = self.vertex(super(), x+start_index).estimate()
